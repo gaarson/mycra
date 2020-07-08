cb624a8cdcef41d88c7d726d4050777122737e35
Ruslan Idrisov <gaarson666@gmail.com>
2020-07-08 20:13:45 +0300

    change Icon component to function Componet

--- utils/icon.js
+++ utils/icon.js
@@ -1,16 +1,16 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 
-export default class Icon extends PureComponent {
-  render() {
-    const {className, glyph, ...restProps} = this.props;
-
-    return React.createElement('svg', { className, ...restProps }, 
-      React.createElement('use', { xlinkHref: `#${glyph}` })
-    );
-  }
-}
+const Icon = ({ className, glyph, ...restProps }) => {
+  return React.createElement(
+    'svg', 
+    { className, ...restProps }, 
+    React.createElement('use', { xlinkHref: `#${glyph}` })
+  );
+};
 
 Icon.defaultProps = {
   glyph: '',
   className: 'icon'
 };
+
+export default Icon;
