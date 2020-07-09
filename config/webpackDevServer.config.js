c783747d6691050bdf503fe5e772c0faf8fca886
Ruslan Idrisov <gaarson666@gmail.com>
2020-07-09 08:36:03 +0300

    fix generated svg component, add static public to dev server

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -9,6 +9,9 @@ module.exports = {
   hot: true,
   historyApiFallback: true,
   open: true,
+  before(app) {
+    app.use('/', express.static(dir.public));
+  },
   stats: {
     colors: true,
   },
