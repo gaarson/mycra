c8f13fd87a1610b51cbd33ba26a6e980e8b4b74f
Alexander Kizilov <kizilov.alex1823@gmail.com>
2021-12-14 17:38:36 +0300

    function for create assets.json

--- scripts/prod.js
+++ scripts/prod.js
@@ -1,5 +1,18 @@
 const webpack = require('webpack');
 const dir = require('../config/paths.js');
+const fs = require('fs');
+const path = require('path');
+const args = require('../utils/args.js');
+
+const createAssetsJson = (assetsList, fileName = 'assets.json') => {
+  fs.writeFile(path.join(dir.dist, fileName), JSON.stringify(assetsList), (err) => {
+    if (err) {
+      console.log(`File with assets "${fileName}" is NOT created`);
+      throw err;
+    }
+    console.log(`File with assets "${fileName}" is created`);
+  });
+};
 
 require('dotenv').config({ path: dir.env });
 
@@ -8,6 +21,14 @@ const prodConfig = require('../config/webpack.config');
 const startBuild = () => new Promise((resolve, reject) => {
   webpack(prodConfig).run((err, stats) => {
     if (err || stats.hasErrors()) reject(stats.compilation.errors);
+
+    if (args.pwa) {
+      createAssetsJson([
+        ...Object.keys(stats.compilation.assets),
+        './'
+      ]);
+    }
+
     resolve();
   });
 });
