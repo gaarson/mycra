1ecabe5b215427f77859fa8ca331a1ab44bd4e54
Ruslan Idrisov <ruslan@markeaze.com>
2024-01-06 06:03:01 +0300

    few changes

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,8 +3,11 @@ import path from 'path';
 import paths from './config/paths.js';
 import args from './utils/args.js';
 import env from './config/env.js';
+import { createRequire } from 'node:module';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const require = createRequire(import.meta.url);
 
 export default {
   rootDir: paths.root,
@@ -19,8 +22,8 @@ export default {
     // '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$',
   ],
   transform: {
-    '^.+\\.(ts|js|jsx|tsx)$': path.join(__dirname, './node_modules/esbuild-runner/jest.js'),
-    '^.+\\.svg$': path.join(__dirname, './node_modules/jest-transformer-svg/lib/index.js'),
+    '^.+\\.(ts|js|jsx|tsx)$': require.resolve('esbuild-runner/jest.js'),
+    '^.+\\.svg$': require.resolve('jest-transformer-svg'),
     '^.+\\.css$': `${path.join(__dirname, '/config')}/jest/css.js`,
     '^.+\?raw$': `${path.join(__dirname, '/config')}/jest/css.js`,
     '^(?!.*\\.(js|jsx|css|json)$)': `${path.join(__dirname, '/config')}/jest/file.js`,
@@ -32,7 +35,7 @@ export default {
   moduleNameMapper: {
     "@/(.*)": paths.app,
     '\\.svg': `${path.join(__dirname, '/__mocks__')}/fileMock.ts`,
-    '^.+\?raw$': path.join(__dirname, './node_modules/jest-raw-loader/lib/index.js'),
+    '^.+\?raw$': require.resolve('jest-raw-loader'),
   },
   testEnvironment: 'jsdom',
   testEnvironmentOptions: {
