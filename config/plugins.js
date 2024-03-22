5f727564b245f0cea901d143424a86a5cbca0ad4
Ruslan Idrisov <ruslan@markeaze.com>
2024-03-22 18:08:56 +0300

    add injecting css into page from js

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
+import { pathToFileURL } from 'url';
 import path from 'path';
-import svgr from 'esbuild-plugin-svgr';
 import envFilePlugin from 'esbuild-envfile-plugin';
 import stylePlugin from 'esbuild-style-plugin'
 import { environmentPlugin } from 'esbuild-plugin-environment';
@@ -10,8 +10,9 @@ import babel from 'esbuild-plugin-babel';
 import { dtsPlugin } from "esbuild-plugin-d.ts";
 import { polyfillNode } from "esbuild-plugin-polyfill-node";
 import { styleNamePlugin } from '../esbuild-module-style-name-plugin/index.js';
+import { stylePerPlugin } from '../esbuild-style-plugin/index.js';
 import buildMode from './buildMode.js';
-import { mySvg } from '../esbuild-my-svg-plugin/index.js';
+import { mySvg } from '../esbuild-svg-plugin/index.js';
 
 import dir from './paths.js';
 import args from '../utils/args.js';
@@ -19,6 +20,15 @@ import progress from 'esbuild-plugin-progress';
 import time from 'esbuild-plugin-time';
 
 let babelConfig;
+
+function buildPath(path, params) {
+    const {id, code, ...query} = params;
+    return path.replace(/\[id]/, id)
+               .replace(/\[code]/, code) +
+               "?" +
+               Object.entries(query).map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+               .join("&");
+}
 
 export const getPlugins = async (scopeGenerator) => { 
   if (args.babel) {
@@ -35,18 +45,21 @@ export const getPlugins = async (scopeGenerator) => {
         config: babelConfig
       })
     ] : []),
-    progress(),
+    // progress(),
     time(),
-    stylePlugin({
+    stylePerPlugin({
       cssModulesMatch: /\.s?css$/,
       renderOptions: {
         sassOptions: {
-          style: 'compressed',
-          importer: (url, prev) => {
-            if (url.startsWith('@/')) {
-              return { file: path.join(dir.app, url.replace('@', '')) };
-            };
-          } 
+          importers: [{
+            findFileUrl(url, ...rest) {
+              if (url.startsWith('@'))  {
+                const resultUrl = new URL(pathToFileURL(path.join(dir.app, url.replace('@', ''))));
+                return resultUrl;
+              }
+              return null
+            }
+          }],
         }
       },
       cssModulesOptions: {
